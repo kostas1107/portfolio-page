6af69788944457578a69379eda3ace7bc6c7b399
Kostas Sudzius <kostas1107@gmail.com>
2021-09-29 16:46:08 +0300

    added functional theme switcher

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const mainWindow = document.getElementById('window');
+const windowHeader = document.getElementById('window-header');
 const aboutTab = document.getElementById('about-tab');
 const projectsTab = document.getElementById('projects-tab');
 const contactTab = document.getElementById('contact-tab');
+const switcher = document.getElementById('header-switcher');
+const circle = document.getElementById('switcher-circle');
+
+let moved = false;
+let aboutTabActive = true;
+let projectsTabActive = false;
+let contactTabActive = false;
 
 const aboutPage = document.getElementById('about-pg');
 const projectsPage = document.getElementById('projects-pg');
@@ -11,31 +20,105 @@ const nameInput = document.getElementById('name-input');
 const emailInput = document.getElementById('email-input');
 const messageField = document.getElementById('message-field');
 
+
+// TAB CONTROL
 aboutTab.addEventListener('click', function () {
+  aboutTabActive = true;
+  projectsTabActive = false;
+  contactTabActive = false;
   aboutPage.classList.add('show');
   projectsPage.classList.remove('show');
   contactPage.classList.remove('show');
-  aboutTab.classList.add('active');
-  projectsTab.classList.remove('active');
-  contactTab.classList.remove('active');
+  if (moved && aboutTabActive) {
+    aboutTab.classList.add('active-light');
+    projectsTab.classList.remove('active-light');
+    contactTab.classList.remove('active-light');
+  } else {
+    aboutTab.classList.add('active');
+    projectsTab.classList.remove('active');
+    contactTab.classList.remove('active');
+  }
 });
 projectsTab.addEventListener('click', function () {
+  aboutTabActive = false;
+  projectsTabActive = true;
+  contactTabActive = false;
   projectsPage.classList.add('show');
   aboutPage.classList.remove('show');
   contactPage.classList.remove('show');
-  projectsTab.classList.add('active');
-  aboutTab.classList.remove('active');
-  contactTab.classList.remove('active');
+  if (!moved && projectsTabActive) {
+    projectsTab.classList.add('active');
+    aboutTab.classList.remove('active');
+    contactTab.classList.remove('active');
+  } else {
+    projectsTab.classList.add('active-light');
+    aboutTab.classList.remove('active-light');
+    contactTab.classList.remove('active-light');
+  }
 });
 contactTab.addEventListener('click', function () {
+  aboutTabActive = false;
+  projectsTabActive = false;
+  contactTabActive = true;
   contactPage.classList.add('show');
   aboutPage.classList.remove('show');
   projectsPage.classList.remove('show');
-  contactTab.classList.add('active');
-  aboutTab.classList.remove('active');
-  projectsTab.classList.remove('active');
+  if (!moved && contactTabActive) {
+    contactTab.classList.add('active');
+    aboutTab.classList.remove('active');
+    projectsTab.classList.remove('active');
+  } else {
+    contactTab.classList.add('active-light');
+    aboutTab.classList.remove('active-light');
+    projectsTab.classList.remove('active-light');
+  }
 });
 
+// THEME SWITCHER
+switcher.addEventListener('click', function () {
+  if (!moved) {
+    circle.classList.remove('move-left');
+    circle.classList.add('move-right');
+    windowHeader.classList.add('light-theme');
+    mainWindow.classList.add('light-theme');
+    aboutTab.classList.add('light-tab');
+    projectsTab.classList.add('light-tab');
+    contactTab.classList.add('light-tab');
+    if (aboutTabActive) {
+      aboutTab.classList.add('active-light');
+    } else if (projectsTabActive) {
+      projectsTab.classList.add('active-light');
+    } else if (contactTabActive) {
+      contactTab.classList.add('active-light');
+    }
+    aboutTab.classList.remove('active');
+    projectsTab.classList.remove('active');
+    contactTab.classList.remove('active');
+    moved = true;
+  } else if (moved) {
+    circle.classList.remove('move-right');
+    circle.classList.add('move-left');
+    windowHeader.classList.remove('light-theme');
+    mainWindow.classList.remove('light-theme');
+    aboutTab.classList.remove('light-tab');
+    projectsTab.classList.remove('light-tab');
+    contactTab.classList.remove('light-tab');
+    if (aboutTabActive) {
+      aboutTab.classList.add('active');
+    } else if (projectsTabActive) {
+      projectsTab.classList.add('active');
+    } else if (contactTabActive) {
+      contactTab.classList.add('active');
+    }
+    aboutTab.classList.remove('active-light');
+    projectsTab.classList.remove('active-light');
+    contactTab.classList.remove('active-light');
+    moved = false;
+  }
+});
+
+
+// FORM SUBMISSION
 async function handleSubmit(event) {
   event.preventDefault();
   const data = new FormData(event.target);
